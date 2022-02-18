10afb97cf9e9935061ea73d0fc4dcfefb168cd9b
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2022-02-18 05:39:38 +0000

    rename variables; update to clap 3.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
 
-use clap::{App, Arg};
+use clap::{Command, Arg};
 
 use csvsqllib as csvsql;
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let app = App::new("csvsql")
+    let app = Command::new("csvsql")
         .version("0.1.0")
         .author("Michael P. Moran")
         .about("Query a CSV file using SQL.")
@@ -23,13 +23,13 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .takes_value(true)
                 .help("SQL query"),
         );
-    let mut app2 = app.clone();
+    let mut app_clone = app.clone();
     let matches = app.get_matches();
     let file = match matches.value_of("file") {
         Some(file) => file,
         None => {
             log::error!("You didn't provide a CSV path.");
-            app2.print_help()?;
+            app_clone.print_help()?;
             return Ok(());
         }
     };
@@ -37,7 +37,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Some(query) => query,
         None => {
             log::error!("You didn't provide a SQL query.");
-            app2.print_help()?;
+            app_clone.print_help()?;
             return Ok(());
         }
     };
